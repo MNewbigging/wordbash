dae435af90bc3640d86e887ff52a9011be566c87
MNewbigging <mrnewbigging@gmail.com>
2021-08-20 21:02:12 +0100

    restarting with separate web and mobile apps

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,20 @@
 import { observer } from 'mobx-react';
 import React from 'react';
 
-import { HomeScreen } from './components/home/HomeScreen';
-import { WordBashScreen, WordBashState } from './state/WordBashState';
+import { ViewMode, WordBashState } from './state/WordBashState';
+import { WordBashWeb } from './components/web/WordBashWeb';
 
 import './app-styles.scss';
 
-/**
- * This higher-order component determines whether to render a mobile or web view,
- * depending on the size of the user's screen. It does so on load and on resize,
- * such that the user could resize their desktop window to see a mobile view.
- */
 @observer
 export class App extends React.PureComponent {
   private readonly wbState = new WordBashState();
 
   public render() {
-    switch (this.wbState.screen) {
-      case WordBashScreen.HOME:
-        return <HomeScreen />;
-      case WordBashScreen.GAME:
-        return <div>game scren</div>;
-      default:
-        return <div>oops</div>;
+    if (this.wbState.viewMode === ViewMode.DESKTOP) {
+      return <WordBashWeb wbState={this.wbState} />;
     }
+
+    return <div></div>;
   }
 }
---
+++ src/components/web/WordBashWeb.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { WordBashState } from '../../state/WordBashState';
+
+interface Props {
+  wbState: WordBashState;
+}
+
+export class WordBashWeb extends React.Component<Props> {
+  public render() {
+    return <div></div>;
+  }
+}
