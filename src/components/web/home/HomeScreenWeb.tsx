ce123c5e541729c6cd5014f8b014a46fa4030484
MNewbigging <mrnewbigging@gmail.com>
2021-08-20 21:27:10 +0100

    home screen animated

--- src/components/web/home/HomeScreenWeb.tsx
+++ src/components/web/home/HomeScreenWeb.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { LetterTile } from '../../common/letter-tile/LetterTile';
 
 import './home-screen-web.scss';
 
@@ -6,7 +7,20 @@ export class HomeScreenWeb extends React.Component {
   public render() {
     return (
       <div className={'home-screen-web'}>
-        <div className={'title'}></div>
+        <div className={'title'}>
+          <div className={'row top'}>
+            <LetterTile letter={'W'} />
+            <LetterTile letter={'O'} />
+            <LetterTile letter={'R'} />
+            <LetterTile letter={'D'} />
+          </div>
+          <div className={'row bot'}>
+            <LetterTile letter={'B'} />
+            <LetterTile letter={'A'} />
+            <LetterTile letter={'S'} />
+            <LetterTile letter={'H'} />
+          </div>
+        </div>
         <div className={'play'}>play</div>
       </div>
     );
