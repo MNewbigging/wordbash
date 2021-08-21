1011d7f7afd428909bd62d039abebcdb459782d5
MNewbigging <mrnewbigging@gmail.com>
2021-08-21 12:24:36 +0100

    letters animating

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import { ViewMode, WordBashState } from './state/WordBashState';
 import { WordBashWeb } from './components/web/WordBashWeb';
 
-import './app-styles.scss';
+import './app.scss';
 
 @observer
 export class App extends React.PureComponent {
---
+++ src/components/web/game/LetterPool.tsx
@@ -0,0 +1,34 @@
+import { observer } from 'mobx-react';
+import React from 'react';
+
+import { Letter } from '../../../utils/LetterGenerator';
+import { LetterTile } from '../../common/letter-tile/LetterTile';
+
+import './letter-pool.scss';
+
+interface Props {
+  letterPool: Letter[];
+}
+
+@observer
+export class LetterPool extends React.Component<Props> {
+  public render() {
+    const { letterPool } = this.props;
+
+    const animDelayStep = 0.1;
+    const maxAnimDelay = letterPool.length * animDelayStep;
+
+    return (
+      <div className={'letter-pool'}>
+        {letterPool.map((letter, i) => (
+          <div
+            className={'letter-container'}
+            style={{ animationDelay: `${maxAnimDelay - i * animDelayStep}s` }}
+          >
+            <LetterTile letter={letter.letter} />
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
