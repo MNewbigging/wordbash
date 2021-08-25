f71aafeb5c26b0a53106974c5a41f4c8e5bd7472
MNewbigging <mrnewbigging@gmail.com>
2021-08-25 16:24:45 +0100

    help dialog content

--- src/components/web/game/HelpDialog.tsx
+++ src/components/web/game/HelpDialog.tsx
@@ -19,7 +19,20 @@ export class HelpDialog extends React.Component<Props> {
 
     return (
       <div className={'help-dialog ' + status}>
-        <div className={'content'}>Hi I'm help </div>
+        <div className={'content'}>
+          <div className={'title'}>How to play</div>
+          <p>The aim of Word Bash is to use all the letters to make as few words as possible!</p>
+
+          <p>
+            You can only use each letter once. Words you have written will appear below the answer
+            input area. You can click on these words to remove them and return their letters to be
+            reused.
+          </p>
+          <p>
+            Answers appear with different colours based on how long the word is. Longer answers mean
+            fewer words used which means a better score!
+          </p>
+        </div>
 
         <div className={'close button'} onClick={() => onClose()}>
           Got it
