f4ac3cbf068033a2bd8f0f7186389cdcc58f2d7e
elsalindholm <31487907+elsalindholm@users.noreply.github.com>
2021-09-14 11:07:34 +0100

    Update GameScreenMobile.tsx

--- src/components/mobile/game/GameScreenMobile.tsx
+++ src/components/mobile/game/GameScreenMobile.tsx
@@ -1,8 +1,11 @@
 import { observer } from 'mobx-react';
-import { ObservableObjectAdministration } from 'mobx/lib/internal';
 import React from 'react';
 
+import { Letter } from '../../../model/Letter';
 import { GameState } from '../../../state/GameState';
+import { HelpDialog } from '../../web/game/HelpDialog';
+import { LetterPool } from '../../web/game/LetterPool';
+import { Navbar } from '../../web/game/Navbar';
 
 interface Props {
   gameState: GameState;
@@ -14,6 +17,29 @@ export class GameScreenMobile extends React.PureComponent<Props> {
   render() {
     const { gameState, onQuit } = this.props;
 
-    return <div className={'game-screen-mobile'}>Game Screen</div>;
+    return (
+      <div className={'game-screen-mobile'}>
+        <HelpDialog
+          status={gameState.helpDialogStatus}
+          onClose={() => gameState.closeHelpDialog()}
+        />
+
+        <div className={'score-area'}>
+          <Navbar
+            wordCount={gameState.acceptedAnswers.length}
+            onQuit={onQuit}
+            onHelp={() => gameState.openHelpDialog()}
+          />
+        </div>
+
+        <div className={'letter-pool-area'}>
+          <LetterPool
+            letterPool={gameState.letterPool}
+            onSelectLetter={(letter: Letter) => gameState.selectLetter(letter)}
+            exitAnim={gameState.gameOver}
+          />
+        </div>
+      </div>
+    );
   }
 }
