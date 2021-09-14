62b8fbe73b51c87d959650feef7b686a5d241cee
elsalindholm <31487907+elsalindholm@users.noreply.github.com>
2021-09-14 18:20:22 +0100

    Game screen worked on

--- src/components/mobile/game/GameScreenMobile.tsx
+++ src/components/mobile/game/GameScreenMobile.tsx
@@ -2,10 +2,16 @@ import { observer } from 'mobx-react';
 import React from 'react';
 
 import { Letter } from '../../../model/Letter';
-import { GameState } from '../../../state/GameState';
+import { Answer, GameState } from '../../../state/GameState';
+import { AnswerInput } from '../../web/game/AnswerInput';
+import { AnswersList } from '../../web/game/AnswersList';
 import { HelpDialog } from '../../web/game/HelpDialog';
 import { LetterPool } from '../../web/game/LetterPool';
 import { Navbar } from '../../web/game/Navbar';
+
+import './game-screen-mobile.scss';
+import './navbar-mobile.scss';
+import './letter-pool-mobile.scss';
 
 interface Props {
   gameState: GameState;
@@ -37,6 +43,22 @@ export class GameScreenMobile extends React.PureComponent<Props> {
             letterPool={gameState.letterPool}
             onSelectLetter={(letter: Letter) => gameState.selectLetter(letter)}
             exitAnim={gameState.gameOver}
+          />
+        </div>
+
+        <div className={'answer-input-area'}>
+          <AnswerInput
+            answerWord={gameState.answerWord}
+            answerStatus={gameState.answerStatus}
+            onAccept={() => gameState.validateAnswerWord()}
+            onRemoveLetter={(letter: Letter) => gameState.removeAnswerLetter(letter)}
+          />
+        </div>
+
+        <div className={'answers-area'}>
+          <AnswersList
+            answers={gameState.acceptedAnswers}
+            onRemove={(answer: Answer) => gameState.removeAnswer(answer)}
           />
         </div>
       </div>
